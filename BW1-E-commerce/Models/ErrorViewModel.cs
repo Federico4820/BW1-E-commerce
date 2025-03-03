3878dafb033eb446c84a9d0f12265b17c8f1f1a9
Federico4820 <federicolepore1@gmail.com>
2025-03-03 10:48:01 +0100

    Aggiungere i file di progetto.

---
+++ BW1-E-commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BW1_E_commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
